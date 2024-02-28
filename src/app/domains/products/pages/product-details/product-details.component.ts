d7462277586385ddc53c4db7d5fbc676c407ba60
EliasMN <elias010493@gmail.com>
2024-02-28 18:23:34 +0100

    Product details terminado

--- src/app/domains/products/pages/product-details/product-details.component.ts
+++ src/app/domains/products/pages/product-details/product-details.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, inject, signal } from '@angular/core';
+import { Product } from '@shared/models/product.model';
+import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
@@ -12,18 +15,35 @@ export default class ProductDetailsComponent {
 
   // La ? es para indicar que puede o no venir un Input
   @Input() id?: string;
-  private productService = inject(ProductService)
+  private productService = inject(ProductService);
+
+  private cartService = inject(CartService);
+
+  product = signal<Product | null>(null);
+  principal = signal('');
+
   ngOnInit() {
     if (this.id) {
-      console.log(this.id);
       this.productService.getOneByID(this.id).subscribe({
         next: (data) => {
-          console.log(data);
+          this.product.set(data);
+          this.principal.set(data.images[0]);
         },
         error: (e) => {
           console.log(e);
         }
       })
+    }
+  }
+
+  cambiarPrincipal(newImage: string) {
+    this.principal.set(newImage);
+  }
+
+  addProduct() {
+    let product = this.product();
+    if (product) {
+      this.cartService.addProduct(product);
     }
   }
 
